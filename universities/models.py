04ba97023eb4f64eb5671e3540ebdbaaf426c7ac
Kade Robertson <kade.robertson16@gmail.com>
2018-01-12 08:15:45 -0500

    expand gitignore, make university model

---
+++ universities/models.py
@@ -0,0 +1,8 @@
+class University():
+    def __init__(self, name, domains, webpages, country_code, stateprov, country):
+        self.name = name
+        self.domains = domains
+        self.webpages = webpages
+        self.country_code = country_code
+        self.stateprov = stateprov
+        self.country = country
